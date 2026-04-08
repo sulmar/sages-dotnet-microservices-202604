d4c81ebee711baa57c11fdee44bbf78e4b783e90
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-04-08 12:03:49 +0200

    feat: Dodanie autoryzacji do Gateway Api

--- src/ApiGateway/YarpApiGateway/Program.cs
+++ src/ApiGateway/YarpApiGateway/Program.cs
@@ -4,7 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication("Bearer");
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("LoggedPolicy", policy => policy.RequireAuthenticatedUser());
+});
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapReverseProxy();
 

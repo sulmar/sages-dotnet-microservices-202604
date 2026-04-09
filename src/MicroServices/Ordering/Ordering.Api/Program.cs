364f363335beef61e07b1e733939bb5bac312dd3
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-04-09 10:13:56 +0200

    docs: Dodanie sprawdzania dostepnosci produktu za pomoca gRPC

--- src/MicroServices/Ordering/Ordering.Api/Program.cs
+++ src/MicroServices/Ordering/Ordering.Api/Program.cs
@@ -2,12 +2,35 @@ using NanoidDotNet;
 using Ordering.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpcClient<Stock.Api.StockService.StockServiceClient>(options => options.Address = new Uri("https://localhost:7118"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello Ordering Api!");
 
-app.MapPost("/api/orders", (Order order) =>
-{
+app.MapPost("/api/orders", async (Order order, Stock.Api.StockService.StockServiceClient stockClient) =>
+{    
+    foreach (var item in order.Items)
+    {
+        var request = new Stock.Api.CheckAvailabilityRequest
+        {
+            ProductId = item.ProductId,
+            Quantity = item.Quantity
+        };
+
+        var response =  stockClient.CheckAvailability(request);
+        
+        if (!response.IsAvailable)
+        {
+            return Results.BadRequest($"Product {item.ProductId} is not available in the requested quantity.");
+        }
+    }
+
+        
+
+
+
     // Here you would typically save the order to a database
     // For this example, we'll just return the order back to the client
     // order.Id = Guid.NewGuid().ToString();
---
+++ src/MicroServices/Stock/Stock.Api/Services/StockGrpcService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+
+namespace Stock.Api.Services;
+
+public class StockGrpcService : StockService.StockServiceBase
+{
+    private readonly Dictionary<int, int> _stock = new()
+    {
+        { 1, 10 },
+        { 2, 5  },
+        { 3, 1 }
+    };
+
+    public override Task<CheckAvailabilityResponse> CheckAvailability(CheckAvailabilityRequest request, ServerCallContext context)
+    {
+        // ...        
+        var available = _stock.TryGetValue(request.ProductId, out var quantity) && quantity >= request.Quantity;
+
+        var response = new CheckAvailabilityResponse
+        {
+            IsAvailable = available
+        };
+
+        return Task.FromResult(response);
+
+    }
+}

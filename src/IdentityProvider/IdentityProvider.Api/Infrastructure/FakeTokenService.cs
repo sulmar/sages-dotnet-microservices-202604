f3f8c9946a4652ff8745a1d6029225d85752f5cf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-04-10 14:05:43 +0200

    feat: Dodanie generowania tokenu JWT

--- src/IdentityProvider/IdentityProvider.Api/Infrastructure/FakeTokenService.cs
+++ src/IdentityProvider/IdentityProvider.Api/Infrastructure/FakeTokenService.cs
@@ -1,11 +1,38 @@
 ﻿using IdentityProvider.Api.Domain;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 namespace IdentityProvider.Api.Infrastructure;
+
+// dotnet add package Microsoft.IdentityModel.JsonWebTokens
 
 public class FakeTokenService : ITokenService
 {
     public string GenerateToken(UserIdentity identity)
     {
-        return "abc";
+        var claims = new Dictionary<string, object>
+        {
+            { JwtRegisteredClaimNames.Name, identity.Username },            
+            { JwtRegisteredClaimNames.Email, identity.Email },
+            { JwtRegisteredClaimNames.GivenName, identity.FirstName },
+            { JwtRegisteredClaimNames.FamilyName, identity.LastName },
+            { JwtRegisteredClaimNames.PhoneNumber, identity.Phone  },
+            { ClaimTypes.Role , identity.Roles }
+        };
+
+        var secretKey = "ThisIsASecretKeyForDemoPurposesOnly";
+
+        var descriptor = new SecurityTokenDescriptor
+        {
+            Claims = claims,
+            Expires = DateTime.UtcNow.AddHours(1),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = new JsonWebTokenHandler().CreateToken(descriptor);
+
+        return token;
     }
 }
--- src/IdentityProvider/IdentityProvider.Api/Infrastructure/FakeUserRepository.cs
+++ src/IdentityProvider/IdentityProvider.Api/Infrastructure/FakeUserRepository.cs
@@ -6,10 +6,10 @@ public class FakeUserRepository : IUserRepository
 {
     private readonly Dictionary<string, UserIdentity> _users = new Dictionary<string, UserIdentity>
     {
-        { "john", new UserIdentity { Username = "john.doe", FirstName = "John", LastName="Smith", HashedPassword = "password123" } },
-        { "jane", new UserIdentity { Username = "jane.smith", FirstName="Jane ",  LastName="Smith", HashedPassword = "password456" } }
+        { "john", new UserIdentity { Username = "john.doe", FirstName = "John", LastName="Smith", Email = "john.doe@example.com", Phone="555-123-000" , HashedPassword = "password123" } },
+        { "jane", new UserIdentity { Username = "jane.smith", FirstName="Jane ",  LastName="Smith", Email = "jane.smith@example.com", Phone= "555-456-000", HashedPassword = "password456" } }
     };
-
+    
     public Task<UserIdentity?> GetByUsernameAsync(string username)
     {
         _users.TryGetValue(username, out var user);
